ae52558b7b73dc9d8dab398b5cf2822af25b5dc8
Raghuveer Singh <raghu1213@hotmail.com>
2018-07-22 16:57:43 +0800

    Initial commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/challenge', challenge)
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler`
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
--- routes/challenge.js
+++ routes/challenge.js
@@ -7,18 +7,5 @@ router.post('/', function (req, res, next) {
     res.send(JSON.stringify(parseInt(input * parseInt(input))));
 });
 
-router.get('/test', async (req, res) => {
-    //res.send(JSON.stringify("Hello World!"));
-    var testData = { input: 2 };
-    await axios.post("https://cis2018studentapp.herokuapp.com/challenge", testData, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(response => {
-        console.log("Response " + JSON.stringify(response.data));
-        res.json(response.data)
-    });
-    console.log("Callback completed");
-});
 
 export default router;
