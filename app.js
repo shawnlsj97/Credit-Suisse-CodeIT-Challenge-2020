1de45aecd994d7d663b2aca75e7675deb075163b
Raghuveer Singh <raghu1213@hotmail.com>
2018-09-16 18:52:52 +0800

    refactor

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var challenge = require('./routes/challenge').default;
+var square = require('./routes/square').default;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -26,8 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/challenge', challenge)
+app.use('/square', square)
 
 // catch 404 and forward to error handler`
 app.use(function (req, res, next) {
