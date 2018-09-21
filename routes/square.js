a8876b42b84c3c7c1c90b52cdf38f9a727da6ed6
Raghuveer Singh <raghu1213@hotmail.com>
2018-09-21 13:46:16 +0800

    minor

--- routes/square.js
+++ routes/square.js
@@ -4,7 +4,7 @@ var router = Router();
 
 router.post('/', function (req, res, next) {
     var input = req.body['input'];
-    res.send(JSON.stringify(parseInt(input * parseInt(input))));
+    res.send(JSON.stringify(parseInt(input) * parseInt(input)));
 });
 
 
