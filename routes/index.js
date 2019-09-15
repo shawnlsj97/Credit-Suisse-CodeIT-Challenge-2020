8324b1ec22177cef06c996f33d1a96caec72d17a
raghu1213 <raghu1213@hotmail.com>
2019-09-15 19:17:07 +0800

    message change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team-X' });
+  res.render('index', { title: 'This is NodeJs template' });
 });
 
 module.exports = router;
