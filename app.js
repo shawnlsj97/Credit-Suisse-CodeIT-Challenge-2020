3bd4e5917f65491b5de8ba92a6eed4c8462d736f
Shawn Lee <shawnlsj97@gmail.com>
2020-09-26 10:31:40 +0800

    Add contact_trace

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var square = require('./routes/square').default;
+var contactTrace = require('./routes/contact_trace').default;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/square', square)
+app.use('/contact_trace', contactTrace);
 
 // catch 404 and forward to error handler`
 app.use(function (req, res, next) {
---
+++ routes/contact_trace.js
@@ -0,0 +1,54 @@
+import { Router } from "express";
+var router = Router();
+
+function checkNonSilent(first, second) {
+  var len = first.length;
+  var count = 0;
+  var i = 0;
+  for (i = 0; i < len; i++) {
+    if (first[i].charAt(0) == second[i].charAt(0)) {
+      count++;
+    }
+  }
+  if (count >= 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+router.post('/', function (req, res) {
+    var infected = req.body['infected'];
+    var origin = req.body['origin'];
+    var cluster = req.body['cluster'];
+    
+    var result_arr = [];
+
+    // when no mutations
+    if (infected['genome'].localeCompare(origin['genome']) == 0) {
+      for (var i in cluster) {
+        result_arr.push(`${infected['name']} -> ${cluster[i]['name']}`);
+      }
+      result_arr.push(`${infected['name']} -> ${origin['name']}`);
+    } else {
+      var infected_genome = infected['genome'].split("-");
+      var origin_genome = origin['genome'].split("-");
+      var isNonSilent = checkNonSilent(infected_genome, origin_genome);
+      if (isNonSilent) {
+        for (var i in cluster) {
+          result_arr.push(`${infected['name']}* -> ${cluster[i]['name']} -> ${origin['name']}`);
+        }
+      } else {
+        for (var i in cluster) {
+          result_arr.push(`${infected['name']} -> ${cluster[i]['name']} -> ${origin['name']}`);
+        }
+      }
+    }
+
+    let result = JSON.stringify(result_arr);
+    console.log("My result--> %s", result);
+    res.send(result);
+});
+
+
+export default router;
