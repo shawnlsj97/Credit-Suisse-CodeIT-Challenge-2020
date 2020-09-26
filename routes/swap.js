068fa8b1d4334d401535b392f00c43fb40ce7a49
Dominique Ng <dominique@u.nus.edu>
2020-09-26 16:27:32 +0800

    Try to enable receiving 31 HTTP requests

--- routes/swap.js
+++ routes/swap.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 var router = Router();
 var result = { 'order': 0 }
 
-router.post("/", function (req, res) {
-  var input = req.body;
-  result["order"] = input["order"];
-  res.send(result);
-});
+for (let i = 0; i < 31; i++) {
+    router.post("/", function (req, res) {
+    var input = req.body;
+    result["order"] = input["order"];
+    res.send(result);
+    });
+}
 
 export default router;
