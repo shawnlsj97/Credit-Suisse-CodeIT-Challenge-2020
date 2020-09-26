c6b7d8fa89c4001a7f1f52f84eef54175703e2b0
Shawn Lee <shawnlsj97@gmail.com>
2020-09-26 17:12:55 +0800

    go

--- routes/fruit_basket.js
+++ routes/fruit_basket.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 var router = Router();
-var weights = [100, 50, 50];
+var weights = [25, 25, 25];
 
 router.post("/", function (req, res) {
   var input = req.body;
