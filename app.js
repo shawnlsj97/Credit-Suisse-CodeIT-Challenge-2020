be7049bc08a1a3a5b4ca19704ea67c2d483aff9f
Shawn Lee <shawnlsj97@gmail.com>
2020-09-26 20:16:14 +0800

    Update parser

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.text({type:"/"}));
 
 app.use('/', index);
 app.use('/square', square)
