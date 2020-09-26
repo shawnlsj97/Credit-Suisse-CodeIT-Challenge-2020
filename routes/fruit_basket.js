a8b379ab95fd7b3f93eb32b70f884e042fbba130
Dominique Ng <dominique@u.nus.edu>
2020-09-26 13:05:05 +0800

    Stop randomly generating fruit weight

--- routes/fruit_basket.js
+++ routes/fruit_basket.js
@@ -1,19 +1,12 @@
 import { Router } from "express";
 var router = Router();
 
-function generateRandomNo() {
-  var min = 1;
-  var max = 100;
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 router.post("/", function (req, res) {
   var input = req.body;
   var basketWeight = 0;
   for (var fruit in input) {
         var currFruitCount = input[fruit]
-        var currFruitWeight = generateRandomNo();
-        basketWeight += currFruitCount * currFruitWeight;
+        basketWeight += currFruitCount * 100;
   }
   res.send(basketWeight.toString());
 });
