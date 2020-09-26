c69b710d75fbca488749d21d002b780efc8066ce
Shawn Lee <shawnlsj97@gmail.com>
2020-09-26 16:21:37 +0800

    update olympiad

--- routes/olympiad.js
+++ routes/olympiad.js
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import { Router } from "express";
 var router = Router();
 
@@ -5,50 +6,65 @@ router.post('/', function (req, res) {
   console.log(req.body);
     var numBooks = req.body["numberOfBooks"];
     var numDays = req.body["numberOfDays"];
-    var books = req.body["books"];
-    var days = req.body["days"];
+    var books = req.body["books"].sort();
+    var days = req.body["days"].sort();
     var usedBooks = [];
     var usedDays = [];
 
     var i;
     var foundPartner = false;
     for (i = 0; i < parseInt(numBooks); i++) {
-      if (usedBooks.includes(i)) {
+      if (usedBooks.includes(books[i])) {
         continue;
       }
       var curr = books[i];
+      var best_partner = -1;
+      var best_day = 1000;
       var j;
-      for (j = 0; j < parseInt(numBooks); j++) {
-        if (j == i || usedBooks.includes(j)) {
+      for (j = parseInt(numBooks) - 1; j >= 0; j--) {
+        if (j == i || usedBooks.includes(books[j])) {
           continue;
         }
         var k;
-        for (k = 0; k < parseInt(numDays); k++) {
-          if (usedDays.includes(k)) {
+        for (k = parseInt(numDays - 1); k >= 0; k--) {
+          if (usedDays.includes(days[k])) {
             continue;
           }
           if (curr + books[j] < days[k]) {
             foundPartner = true;
-            usedBooks.push(i);
-            usedBooks.push(j);
-            usedDays.push(k);
+            if (books[j] > best_partner) {
+              best_partner = books[j];
+            }
+            if (days[k] < best_day) {
+              best_day = days[k];
+            }
             break;
           }
+        }
+        if (foundPartner) {
+          break;
+        }
+      }
+      if (foundPartner) {
+        if (days.includes(best_day) && books.includes(books[i]) && books.includes(best_partner)) {
+          usedBooks.push(books[i]);
+          usedBooks.push(best_partner);
+          usedDays.push(best_day);
         }
       }
       var best = 1000;
       if (!foundPartner) {
         // best fit algo
         for (k = 0; k < parseInt(numDays); k++) {
-          if (usedDays.includes(k)) {
+          if (usedDays.includes(days[k])) {
             continue;
           }
-          if (k >= books[i] && k < best) {
-            best = k;
+          if (days[k] >= books[i] && k < best) {
+            best = days[k];
           }
         }
         if (days.includes(best)) {
-          usedBooks.push(i);
+          usedBooks.push(books[i]);
           usedDays.push(best);
         }
       }
