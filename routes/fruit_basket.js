4471bba21064d0e4f7476e0c90954d2deb1cd00b
Shawn Lee <shawnlsj97@gmail.com>
2020-09-26 22:34:31 +0800

    Logging

--- routes/fruit_basket.js
+++ routes/fruit_basket.js
@@ -1,20 +1,23 @@
 import { Router } from "express";
 var router = Router();
-var weights = [1, 1, 1];
+var weights = new Map();
+weights.set("maPomegranate", 1);
+weights.set("maPineapple", 1);
+weights.set("maAvocado", 1);
+weights.set("maApple", 1);
+weights.set("maWatermelon", 1);
+weights.set("maRamubutan", 1);
+
 
 router.post("/", function (req, res) {
   var input = JSON.parse(req.body);
-  console.log(req.body);
+  console.log(input);
   var basketWeight = 0;
-  var i = 0;
   var fruit = 0;
   for (fruit in input) {
     var currFruitCount = input[fruit]
-    basketWeight += currFruitCount * weights[i];
-    i++;
+    basketWeight += currFruitCount * weights.get(fruit);
   }
-  console.log(basketWeight);
-  console.log(weights);
   res.send(JSON.stringify(basketWeight));
 });
 
